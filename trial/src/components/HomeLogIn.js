a63e08b6ea8dfdbc82ac9c0471a1037f37b2aea2
denis kadrich <dev4long@live.com>
2023-02-08 15:38:48 -0500

    added sign up

--- trial/src/components/HomeLogIn.js
+++ trial/src/components/HomeLogIn.js
@@ -2,8 +2,18 @@ import React from 'react'
 import "../style/HomeLogIn.css"
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { Link } from '@material-ui/core';
+import {useState} from 'react';
+
 
 function HomeLogIn() {
+
+  const[signUp, toggleSignUp] = useState(false)
+  
+  const switchSignUp = () => toggleSignUp(!signUp)
+
+
   return (
     <div className="LoginContainer">
     
@@ -13,22 +23,51 @@ function HomeLogIn() {
     
     <div className = "formContainer">
       
-      <div className="LoginForm">
+      {signUp ? ( null ) : <div className="LoginForm">
+      <div><LockOutlinedIcon style={{color:"white", background:"red", marginBottom:"20px"}}/></div>
       Login
       <form>
       <div>
-      <TextField label="Email address" variant="outlined" margin="normal" color="secondary"/>
+        <TextField label="Email address *" variant="outlined" margin="normal" color="secondary"/>
       </div>
-      <div>
-        <TextField label="Password" variant="outlined" margin="normal"
+      <div style={{marginBottom:"20px"}}>
+        <TextField label="Password *" variant="outlined" margin="normal"
         color="secondary"/>
       </div>
-      <Button variant="contained" color="secondary" >
-      Submit
-      </Button>
+        <Button style={{marginBottom:"20px"}} variant="contained" color="secondary" >
+        sign in
+        </Button>
+      <div onClick={switchSignUp}>
+      <Link href="#" variant="body2">
+      {"Don't have an account? Sign Up"}
+      </Link>
+      </div>
 
       </form>
+      </div>}
+
+      {signUp ? (<div className="LoginForm">
+      <div><LockOutlinedIcon style={{color:"white", background:"red", marginBottom:"20px"}}/></div>
+      Sign Up
+      <form>
+      <div>
+        <TextField label="Email address *" variant="outlined" margin="normal" color="secondary"/>
       </div>
+      <div style={{marginBottom:"20px"}}>
+        <TextField label="Password *" variant="outlined" margin="normal"
+        color="secondary"/>
+      </div>
+        <Button style={{marginBottom:"20px"}} variant="contained" color="secondary" >
+      sign in
+      </Button>
+      <div onClick={switchSignUp}>
+      <Link href="#" variant="body2">
+      {"Have an account? Sign In"}
+      </Link>
+      </div>
+
+      </form>
+      </div>):null}
       
     </div>
     
